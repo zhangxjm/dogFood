71fd9c779248087ab938d6e7648072a8c5b11a9b
zhangxjm <48610179+zhangxjm@users.noreply.github.com>
2026-05-01 21:06:13 +0800

    Delete EStore directory

--- EStore/client/src/api/auth.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export function login(data) {
-  return request.post('/admin/auth/login', data)
-}
-
-export function getInfo() {
-  return request.get('/admin/info')
-}
