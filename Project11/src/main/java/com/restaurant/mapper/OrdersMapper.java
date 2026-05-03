e6ed9649d995207daac2ba4aeda995cc65740f23
zhangxjm <48610179+zhangxjm@users.noreply.github.com>
2026-05-03 14:53:40 +0800

    Delete Project11 directory

--- Project11/src/main/java/com/restaurant/mapper/OrdersMapper.java
+++
@@ -1,9 +0,0 @@
-package com.restaurant.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.restaurant.entity.Orders;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface OrdersMapper extends BaseMapper<Orders> {
-}
