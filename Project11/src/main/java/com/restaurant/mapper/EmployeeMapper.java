fc961d2e2a9044f7cdcdc6f0a6ed05df2a5684e3
zhangxjm <48610179+zhangxjm@users.noreply.github.com>
2026-05-03 15:22:11 +0800

    Delete Project11 directory

--- Project11/src/main/java/com/restaurant/mapper/EmployeeMapper.java
+++
@@ -1,9 +0,0 @@
-package com.restaurant.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.restaurant.entity.Employee;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface EmployeeMapper extends BaseMapper<Employee> {
-}
