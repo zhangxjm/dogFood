1d08b2617b92b02646ef15c6fb4cd1c094b0ae1e
zhangxjm <48610179+zhangxjm@users.noreply.github.com>
2026-05-04 15:03:36 +0800

    Delete Project28 directory

--- Project28/backend/apps/collection/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class CollectionConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.collection'
-    verbose_name = '收藏管理'
--- Project28/backend/apps/item/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ItemConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.item'
-    verbose_name = '物品管理'
