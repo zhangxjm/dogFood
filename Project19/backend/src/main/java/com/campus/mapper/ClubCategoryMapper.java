e9274803358050d788e0f7e084c466f908d53066
zhangxjm <48610179+zhangxjm@users.noreply.github.com>
2026-05-04 14:05:49 +0800

    Delete Project19 directory

--- Project19/backend/src/main/java/com/campus/mapper/ClubCategoryMapper.java
+++
@@ -1,9 +0,0 @@
-package com.campus.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.campus.entity.ClubCategory;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface ClubCategoryMapper extends BaseMapper<ClubCategory> {
-}
