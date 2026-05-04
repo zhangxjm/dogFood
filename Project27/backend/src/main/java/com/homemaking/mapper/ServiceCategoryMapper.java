d4da9612fd278d0bb7eb990731d16c86234f299a
zhangxjm <48610179+zhangxjm@users.noreply.github.com>
2026-05-04 14:06:04 +0800

    Delete Project27 directory

--- Project27/backend/src/main/java/com/homemaking/mapper/ServiceCategoryMapper.java
+++
@@ -1,9 +0,0 @@
-package com.homemaking.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.homemaking.entity.ServiceCategory;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface ServiceCategoryMapper extends BaseMapper<ServiceCategory> {
-}
